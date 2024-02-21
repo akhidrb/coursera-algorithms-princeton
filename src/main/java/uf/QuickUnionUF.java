c2b516ff3f53969ef0b3f027baf2318400040fe4
ibrahimkhidr <akhidrb@gmail.com>
2024-02-22 00:28:32 +0200

    balanced tree in quick union

--- src/main/java/uf/QuickUnionUF.java
+++ src/main/java/uf/QuickUnionUF.java
@@ -3,17 +3,29 @@ package src.main.java.uf;
 public class QuickUnionUF implements UF {
     int n;
     int[] relation;
+    int[] size;
 
     public QuickUnionUF(int n) {
         this.n = n;
         this.relation = new int[n];
+        this.size = new int[n];
         for (int i = 0; i < n; i++) {
             this.relation[i] = i;
+            this.size[i] = 1;
         }
     }
 
     public void union(int p, int q) {
-        this.relation[getRoot(p)] = getRoot(q);
+        q = getRoot(q);
+        p = getRoot(p);
+        if (size[q] < size[p]) {
+            relation[q] = p;
+            size[p] += size[q];
+        }
+        else {
+            relation[p] = q;
+            size[q] += size[p];
+        }
     }
 
     public boolean connected(int p, int q) {
