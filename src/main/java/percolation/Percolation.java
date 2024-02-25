6bfc751673a6357ce5cd0cbeb887027d3e425964
ibrahimkhidr <akhidrb@gmail.com>
2024-02-25 10:21:55 +0200

    optimize percolation

--- src/main/java/percolation/Percolation.java
+++ src/main/java/percolation/Percolation.java
@@ -1,144 +1,74 @@
 package src.main.java.percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation {
-    // creates n-by-n grid, with all sites initially blocked
+    private boolean[][] grid;
+    private int len;
+    private int top = 0;
+    private int bottom;
+    private WeightedQuickUnionUF uf;
+    private int openSites = 0;
 
-    private int n;
-    private int openSites;
-    private int[] relation;
-    private boolean[] site;
-    private int[] size;
-
-    public Percolation(int n) {
-        if (n < 1) {
-            throw new IllegalArgumentException();
-        }
-        this.n = n;
-        this.openSites = 0;
-        this.site = new boolean[(n * n) + 2];
-        this.relation = new int[(n * n) + 2];
-        this.size = new int[(n * n) + 2];
-        for (int i = 0; i < (n * n) + 2; i++) {
-            this.relation[i] = i;
-            this.size[i] = 1;
-        }
+    public Percolation(int N) {
+        if (N <= 0) throw new IllegalArgumentException();
+        len = N;
+        grid = new boolean[N][N];
+        bottom = N * N + 1;
+        uf = new WeightedQuickUnionUF(N * N + 2);
     }
 
-    // opens the site (row, col) if it is not open already
-    public void open(int row, int col) {
-        int ind = getIndex(row, col);
-        if (this.site[ind]) {
-            return;
+    public void open(int i, int j) {
+        if (i < 1 || i > len || j < 1 || j > len) throw new IllegalArgumentException();
+
+        if (grid[i - 1][j - 1]) return;
+
+        grid[i - 1][j - 1] = true;
+
+        if (i == 1) {
+            uf.union(getSiteIndex(i, j), top);
         }
-        this.site[ind] = true;
+        if (i == len) {
+            uf.union(getSiteIndex(i, j), bottom);
+        }
+
+        if (i > 1 && isOpen(i - 1, j)) {
+            uf.union(getSiteIndex(i, j), getSiteIndex(i - 1, j));
+        }
+        if (i < len && isOpen(i + 1, j)) {
+            uf.union(getSiteIndex(i, j), getSiteIndex(i + 1, j));
+        }
+        if (j > 1 && isOpen(i, j - 1)) {
+            uf.union(getSiteIndex(i, j), getSiteIndex(i, j - 1));
+        }
+        if (j < len && isOpen(i, j + 1)) {
+            uf.union(getSiteIndex(i, j), getSiteIndex(i, j + 1));
+        }
         openSites++;
-        if (ind <= n) {
-            site[0] = true;
-            union(ind, 0);
-        }
-        else if (ind > n * (n - 1)) {
-            site[n * n + 1] = true;
-            union(ind, n * n + 1);
-        }
-        for (int i = 1; i <= 4; i++) {
-            int dirIndex = getIndexFromDir(i, row, col);
-            if (dirIndex == -1 || !site[dirIndex] || connected(ind, dirIndex)) {
-                continue;
-            }
-            union(ind, dirIndex);
-        }
     }
 
-    private boolean connected(int p, int q) {
-        return find(p) == find(q);
+    public boolean isOpen(int i, int j) {
+        return grid[i - 1][j - 1];
     }
 
-    private int getIndexFromDir(int dir, int row, int col) {
-        switch (dir) {
-            case 1:
-                if (row == 1) {
-                    return -1;
-                }
-                row--;
-                break;
-            case 2:
-                if (col == n) {
-                    return -1;
-                }
-                col++;
-                break;
-            case 3:
-                if (row == n) {
-                    return -1;
-                }
-                row++;
-                break;
-            case 4:
-                if (col == 1) {
-                    return -1;
-                }
-                col--;
-                break;
-        }
-        return getIndex(row, col);
+    public boolean isFull(int i, int j) {
+        if (i <= 0 || i > len || j <= 0 || j > len) throw new IndexOutOfBoundsException();
+        return uf.connected(getSiteIndex(i, j), top);
     }
 
-    public int getIndex(int row, int col) {
-        if (row < 1 || row > n || col < 1 || col > n) {
-            throw new IllegalArgumentException();
-        }
-        row--;
-        int ind = (row * n) + col;
-        return ind;
+    public boolean percolates() {
+        return uf.connected(top, bottom);
     }
 
-    private void union(int p, int q) {
-        q = find(q);
-        p = find(p);
-        if (p == q) return;
-
-        if (size[q] < size[p]) {
-            relation[q] = p;
-            size[p] += size[q];
-        }
-        else {
-            relation[p] = q;
-            size[q] += size[p];
-        }
-    }
-
-    public int find(int p) {
-        while (relation[p] != p) {
-            p = relation[p];
-        }
-        return p;
-    }
-
-    // is the site (row, col) open?
-    public boolean isOpen(int row, int col) {
-        int ind = getIndex(row, col);
-        return site[ind];
-    }
-
-    // is the site (row, col) full?
-    public boolean isFull(int row, int col) {
-        int ind = getIndex(row, col);
-        int root = find(ind);
-        return root == 0;
-    }
-
-    // returns the number of open sites
     public int numberOfOpenSites() {
         return openSites;
     }
 
-    // does the system percolate?
-    public boolean percolates() {
-        return connected(0, n * n + 1);
+    private int getSiteIndex(int i, int j) {
+        return len * (i - 1) + j;
     }
 
     public static void main(String[] args) {
 
     }
-
 }
--- src/main/java/percolation/PercolationTest.java
+++ src/main/java/percolation/PercolationTest.java
@@ -46,37 +46,6 @@ public class PercolationTest {
     }
 
     @Test
-    public void p2() {
-        int n = 5;
-        Percolation perc = new Percolation(n);
-        perc.open(4, 3);
-        perc.open(3, 5);
-        perc.open(2, 3);
-        perc.open(1, 3);
-        perc.open(2, 4);
-        perc.open(2, 5);
-        perc.open(4, 4);
-        perc.open(4, 5);
-        Assertions.assertTrue(!perc.percolates());
-        Assertions.assertTrue(perc.isFull(4, 3));
-        perc.open(5, 3);
-        Assertions.assertTrue(perc.percolates());
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                int ind = perc.getIndex(i, j);
-                System.out.print(perc.find(ind) + "\t");
-            }
-            System.out.println();
-        }
-        // 1  2  3  4  5
-        // 6  7  8  9  10
-        // 11 12 13 14 15
-        // 16 17 18 19 20
-        // 21 22 23 24 25
-
-    }
-
-    @Test
     public void stats() {
         int N = 10;
         int T = 100000;
